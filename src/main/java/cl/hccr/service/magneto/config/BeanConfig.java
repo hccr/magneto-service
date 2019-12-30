64bb6e5fa7ac5910e322b2d0e3310025882142f5
Harold Campuzano <harold.campuzano@safecar.cl>
2019-12-30 16:42:43 -0300

    Se agrega actuator para que ver el estado del servidor Se agrega InstanceProfileCredentialsProvider para que obtenga las credenciales AWS directo desde la instancia

--- src/main/java/cl/hccr/service/magneto/config/BeanConfig.java
+++ src/main/java/cl/hccr/service/magneto/config/BeanConfig.java
@@ -2,7 +2,7 @@ package cl.hccr.service.magneto.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
@@ -14,8 +14,7 @@ public class BeanConfig {
 
         SqsClient client = SqsClient.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(ProfileCredentialsProvider.builder()
-                        .profileName("userMercadoLibre")
+                .credentialsProvider(InstanceProfileCredentialsProvider.builder()
                         .build())
                 .build();
         return client;
